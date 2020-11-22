1486329d44bafa7c17c1b3ef0d39f2d0494c925a
Aharon Abadi <34122871+abadiwhitesource@users.noreply.github.com>
2020-11-22 12:05:36 +0200

    first commit

---
+++ core/src/main/resources/hudson/ProxyConfiguration/config.groovy
@@ -0,0 +1,34 @@
+package hudson.ProxyConfiguration
+
+def f=namespace(lib.FormTagLib)
+def l=namespace(lib.LayoutTagLib)
+
+if (!h.hasPermission(app.ADMINISTER)) {
+    set("readOnlyMode", "true")
+}
+
+f.entry(title:_("Server"),field:"name") {
+    f.textbox()
+}
+f.entry(title:_("Port"),field:"port") {
+    f.number(clazz:"number",min:0,max:65535,step:1)
+}
+f.entry(title:_("User name"),field:"userName") {
+    f.textbox()
+}
+f.entry(title:_("Password"),field:"secretPassword") {
+    f.password()
+}
+f.entry(title:_("No Proxy Host"),field:"noProxyHost") {
+    f.textarea()
+}
+
+l.isAdmin() {
+    f.advanced() {
+        f.entry(title: _("Test URL"), field: "testUrl") {
+            f.textbox()
+        }
+        f.validateButton(title:_("Validate Proxy"),
+                         method:"validateProxy", with:"testUrl,name,port,userName,secretPassword,noProxyHost")
+    }
+}
